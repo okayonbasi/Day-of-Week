e45ba6fe2bbfeb77e0b79f32b60ea189dfe26ce3
Okay Onbaşı <okayonbasi@gmail.com>
2019-03-25 23:03:39 +0300

    day of week with Disparate variation

---
+++ DayOfWeek/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DayOfWeek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Day";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate For Disparate Variation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 154);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Disparate Variation";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(365, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 154);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kraitchik\'s Variation";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(100, 91);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 91);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Result :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Calculate For Kraitchik\'s Variation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 229);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DayOfWeek/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayOfWeek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Disparate Variation */
+
+        public int takeMount(int mount) 
+        {
+            if (mount == 1)
+            {
+                return 11;
+            }
+            else if (mount == 2)
+            {
+                return 12;
+            }
+            else return mount -2;
+        }
+
+        public int takeYear(int year, int mount) 
+        {
+            int y = 0;
+            year = newYear(year, mount);
+            if (year % 100 == 0)
+            {
+                y = 100;
+                return y;
+            }
+            else 
+            {
+                y = year % 100;
+                return y;
+            }
+        }
+
+        public int takeCentury(int year, int mount)
+        {
+            year = newYear(year,mount);
+            int century = year / 100;
+            return century;
+        }
+
+        public int newYear(int year, int mount) 
+        {
+            if (mount == 1 || mount == 2)
+            {
+                year = year - 1;
+                return year;
+            }
+            else
+            {
+                return year;
+            }
+            
+        }
+
+
+        public int findDayWithDisparate(int day, int mount, int year) 
+        {
+            int century = takeCentury(year,mount);
+            int d;
+            d = Convert.ToInt32((day + Math.Floor((2.6 * takeMount(mount)) - 0.2) + takeYear(year,mount) + (takeYear(year,mount) / 4 )+ (century / 4 - (2 * century))) % 7);
+            return d;
+        }
+
+        public string TakeDay(int x) 
+        {
+            string day;
+            switch(x)
+            {
+                case 0: day = "Pazar"; break;
+                case 1: day = "Pazartesi"; break;
+                case 2: day = "Salı"; break;
+                case 3: day = "Çarşamba"; break;
+                case 4: day = "Perşembe"; break;
+                case 5: day = "Cuma"; break;
+                case 6: day = "Cumartesi"; break;
+                default: day = "wrong"; break;
+            }
+            return day;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int day = Convert.ToInt32(textBox1.Text);
+            int mount = Convert.ToInt32(textBox2.Text);
+            int year = Convert.ToInt32(textBox3.Text);
+
+            int findOfWeek = findDayWithDisparate(day,mount,year);
+            label5.Text = TakeDay(findOfWeek);
+            label5.Visible = true;
+                
+        }
+
+        /* Kraitchik's Variation */
+
+        /****************************************************************************************************************/
+        public int tMount(int m) 
+        {
+            int[] mount = { 1, 4, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+            return mount[m - 1];
+        }
+
+        public int tCentury(int c)
+        {
+            int[] century = { 0, 5, 3, 1 };
+            return century[c];        }
+
+
+        public string tDay(int x) 
+        {
+            string day;
+            switch (x)
+            {
+                case 1: day = "Pazar"; break;
+                case 2: day = "Pazartesi"; break;
+                case 3: day = "Salı"; break;
+                case 4: day = "Çarşamba"; break;
+                case 5: day = "Perşembe"; break;
+                case 6: day = "Cuma"; break;
+                case 0: day = "Cumartesi"; break;
+                default: day = "wrong"; break;
+            }
+            return day;
+
+        }
+
+        public Boolean isLeapYear(int year) 
+        {
+            if((year % 400 == 0 || year % 100 != 0) && (year % 4 == 0))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public int tFirst2Digit(int year) 
+        {
+            int x = year / 100;
+            return x;
+        }
+
+        public int tLast2Digit(int year)
+        {
+            int x = year % 100;
+            return x;
+        }
+
+
+        public int findDayWithKraitchiks(int day, int mount, int year)
+        {
+            int calculate = mod(Convert.ToInt32(Math.Floor(tLast2Digit(year) / 4.0) + tLast2Digit(year)), 7);
+            int dayOfWeek = mod((day + tMount(mount) + tCentury((year / 100) % 4) + calculate),7);
+                if (isLeapYear(year))
+                {
+                    if (mount == 1 || mount == 2)
+                    {
+                        if (dayOfWeek == 1)
+                        {
+                            dayOfWeek = 6;
+                        }
+                        else
+                        {
+                            dayOfWeek = dayOfWeek - 2;
+                        }
+                    }
+                    else
+                    {
+                        if (mount == 1 || mount == 2)
+                        {
+                            dayOfWeek = dayOfWeek - 1;
+                        }
+                    }
+                }
+                return dayOfWeek;
+            }
+
+        static int mod(int a, int b)
+        {
+            return Math.Abs(a % b + b) % b;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int day = Convert.ToInt32(textBox1.Text);
+            int mount = Convert.ToInt32(textBox2.Text);
+            int year = Convert.ToInt32(textBox3.Text);
+
+            label10.Text = tDay(findDayWithKraitchiks(day, mount, year));
+            label10.Visible = true;
+
+        
+
+        }
+
+    }
+}
+
+
+
+
+
